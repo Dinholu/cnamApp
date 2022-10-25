2f2284d117a073f9256a02761d0ad8aa1e4c27fb
emmamau <emmanuel.maurice@gmail.com>
2022-10-25 15:09:30 +0200

    Update index.php

--- public/api/index.php
+++ public/api/index.php
@@ -1,2 +1,77 @@
 <?php
-echo ("ok");
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+
+use Tuupola\Middleware\HttpBasicAuthentication;
+use \Firebase\JWT\JWT;
+
+$app = AppFactory::create();
+
+function  addHeaders (Response $response) : Response {
+    $response = $response
+    ->withHeader("Content-Type", "application/json")
+    ->withHeader('Access-Control-Allow-Origin', ('https://tpbackend.herokuapp.com'))
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type,  Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    ->withHeader('Access-Control-Expose-Headers', 'Authorization');
+
+    return $response;
+}
+
+function createJwt (Response $response) : Response {
+    $userid = "emma";
+    $email = "emma@emma.fr";
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 60; // jwt valid for 60 seconds from the issued time
+    $payload = array(
+        'userid' => $userid,
+        'iat' => $issuedAt,
+        'exp' => $expirationTime
+    );
+    $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
+    $response = $response->withHeader("Authorization", "Bearer {$token_jwt}");
+    return $response;
+}
+
+const JWT_SECRET = "MET02-CNAM";
+
+$app->options('/api/auth/{login}', function (Request $request, Response $response, $args) {
+
+    // Evite que le front demande une confirmation à chaque modification
+    $response = $response->withHeader("Access-Control-Max-Age", 600);
+
+    return addHeaders ($response);
+});
+
+// APi d'authentification générant un JWT
+$app->get('/api/login', function (Request $request, Response $response, $args) {   
+    $response = addHeaders ($response);
+    $response = createJwT ($response);
+    $data = array('nom' => "manu", 'prenom' => "maurice");
+    return $response;
+});
+
+// Middleware de validation du Jwt
+$options = [
+    "attribute" => "token",
+    "header" => "Authorization",
+    "regexp" => "/Bearer\s+(.*)$/i",
+    "secure" => false,
+    "algorithm" => ["HS256"],
+    "secret" => JWT_SECRET,
+    "path" => ["/api"],
+    "ignore" => ["/api/hello","/api/login"],
+    "error" => function ($response, $arguments) {
+        $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
+        $response = $response->withStatus(401);
+        return $response->withHeader("Content-Type", "application/json")->getBody()->write(json_encode($data));
+    }
+];
+
+// Chargement du Middleware
+$app->add(new Tuupola\Middleware\JwtAuthentication($options));
+
+// Run app
+$app->run();
