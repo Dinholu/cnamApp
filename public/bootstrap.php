2395de95042e4bdd26c55000a5ec1e5d830d358b
emmamau <emmanuel.maurice@gmail.com>
2022-10-25 22:50:06 +0200

    Update bootstrap.php

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -6,11 +6,11 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'ec2-3-248-4-172.eu-west-1.compute.amazonaws.com',
+'host' => 'dpg-ccuhk2kgqg4a9295cagg-a',
 'driver' => 'pdo_pgsql',
-'user' => 'lmcibctugyfgla',
-'password' => 'd6de1b827bf54f8fcb142f74f76c1d075eade5381e8843a11f8164eda91ce2e0',
-'dbname' => 'd8jrr50unmfhsl',
+'user' => 'cnam_user',
+'password' => '0TJXmePJA9OkhPb6gllHoVZJIL6aOvDS',
+'dbname' => 'cnam',
 'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
