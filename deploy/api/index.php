f8d4ab0a612e02117a840e204bb73dfceb0662a4
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-03-23 15:21:32 +0100

    cors

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -10,6 +10,16 @@ const JWT_SECRET = "makey1234567";
 
 $app = AppFactory::create();
 
+function  addHeaders (Response $response) : Response {
+    $response = $response
+    ->withHeader("Content-Type", "application/json")
+    ->withHeader('Access-Control-Allow-Origin', ('http://localost:4200'))
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type,  Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    ->withHeader('Access-Control-Expose-Headers', 'Authorization');
+
+    return $response;
+}
 
 $app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
     $array = [];
@@ -35,6 +45,14 @@ function createJwT (Response $response) : Response {
     return $response;
 }
 
+$app->options('/api/user', function (Request $request, Response $response, $args) {
+    
+    // Evite que le front demande une confirmation à chaque modification
+    $response = $response->withHeader("Access-Control-Max-Age", 600);
+    
+    return addHeaders ($response);
+});
+
 $app->get('/api/user', function (Request $request, Response $response, $args) {   
     $data = array('nom' => 'toto', 'prenom' => 'titi','adresse' => '6 rue des fleurs', 'tel' => '0606060607');
     $response->getBody()->write(json_encode($data));
@@ -42,6 +60,15 @@ $app->get('/api/user', function (Request $request, Response $response, $args) {
     return $response;
 });
 
+
+
+$app->options('/api/login', function (Request $request, Response $response, $args) {
+    
+    // Evite que le front demande une confirmation à chaque modification
+    $response = $response->withHeader("Access-Control-Max-Age", 600);
+    
+    return addHeaders ($response);
+});
 // APi d'authentification générant un JWT
 $app->post('/api/login', function (Request $request, Response $response, $args) {   
     $err=false;
@@ -67,6 +94,33 @@ $app->post('/api/login', function (Request $request, Response $response, $args) 
 });
 
 
+
+$app->options('/api/catalogue', function (Request $request, Response $response, $args) {
+    
+    // Evite que le front demande une confirmation à chaque modification
+    $response = $response->withHeader("Access-Control-Max-Age", 600);
+    
+    return addHeaders ($response);
+});
+// API Nécessitant un Jwt valide
+$app->get('/api/catalogue/{filtre}', function (Request $request, Response $response, $args) {
+    $filtre = $args['filtre'];
+    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+   
+    if ($filtre) {
+      $data = json_decode($flux, true); 
+    	
+        $res = array_filter($data, function($obj) use ($filtre)
+        { 
+            return strpos($obj["titre"], $filtre) !== false;
+        });
+        $response->getBody()->write(json_encode(array_values($res)));
+    } else {
+         $response->getBody()->write($flux);
+    }
+
+    return $response;
+});
 $options = [
     "attribute" => "token",
     "header" => "Authorization",
