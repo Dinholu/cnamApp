95d42f6e8bed5e23cc276a29e0cd9f555ee72625
Dinholu <alizeehett@hotmail.fr>
2023-11-23 16:36:13 +0100

    Ajout controller

--- deploy/api/controller.php
+++ deploy/api/controller.php
@@ -6,33 +6,33 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 function optionsCatalogue(Request $request, Response $response, $args)
 {
 
-	// Evite que le front demande une confirmation à chaque modification
-	$response = $response->withHeader("Access-Control-Max-Age", 600);
+  // Evite que le front demande une confirmation à chaque modification
+  $response = $response->withHeader("Access-Control-Max-Age", 600);
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
 
 function hello(Request $request, Response $response, $args)
 {
-	$array = [];
-	$array["nom"] = $args['name'];
-	$response->getBody()->write(json_encode($array));
-	return $response;
+  $array = [];
+  $array["nom"] = $args['name'];
+  $response->getBody()->write(json_encode($array));
+  return $response;
 }
 
 function  getSearchCalatogue(Request $request, Response $response, $args)
 {
-	$flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+  $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
 
-	$response->getBody()->write($flux);
+  $response->getBody()->write($flux);
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
 
 // API Nécessitant un Jwt valide
 function getCatalogue(Request $request, Response $response, $args)
 {
-	$flux = '[
+  $flux = '[
   {
     "ref": "x001",
     "designation": "angular",
@@ -95,40 +95,45 @@ function getCatalogue(Request $request, Response $response, $args)
   }
 ]';
 
-	$response->getBody()->write($flux);
+  $response->getBody()->write($flux);
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
 
 function optionsUtilisateur(Request $request, Response $response, $args)
 {
 
-	// Evite que le front demande une confirmation à chaque modification
-	$response = $response->withHeader("Access-Control-Max-Age", 600);
+  // Evite que le front demande une confirmation à chaque modification
+  $response = $response->withHeader("Access-Control-Max-Age", 600);
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
 
 // API Nécessitant un Jwt valide
 function getUtilisateur(Request $request, Response $response, $args)
 {
 
-	$payload = getJWTToken($request);
-	$login  = $payload->userid;
+  $payload = getJWTToken($request);
+  $login  = $payload->userid;
 
-	$flux = '{"nom":"martin","prenom":"jean"}';
+  $flux = '{"nom":"martin","prenom":"jean"}';
 
-	$response->getBody()->write($flux);
+  $response->getBody()->write($flux);
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
 
 // APi d'authentification générant un JWT
 function postLogin(Request $request, Response $response, $args)
 {
-	$flux = '{"nom":"martin","prenom":"jean"}';
-	$response = createJwT($response);
-	$response->getBody()->write($flux);
+  $database = '{"login":"emma","password":"toto"}';
+  if ($request->getBody() == $database) {
+    $flux = '{"nom":"Stones","prenom":"Emma"}';
+    $response = createJwT($response);
+    $response->getBody()->write($flux);
+  } else {
+    $response = $response->withStatus(401);
+  }
 
-	return addHeaders($response);
+  return addHeaders($response);
 }
